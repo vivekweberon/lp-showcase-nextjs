03c6a23e2cf77a08ab8926ebd2a2d41923c43b42
vivekweberon <vivek@weberon.net>
2024-03-20 15:31:23 +0530

    Working on Production errors

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 function MyApp({ Component, pageProps }) {
-  // const basePath = process.env.BASE_PATH || "";
+  const basePath = process.env.BASE_PATH || "";
 
   return (
     <>
