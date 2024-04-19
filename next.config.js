36a40145d8e3929cc31a28ecf571dcd2c8383c64
vivekweberon <vivek@weberon.net>
2024-04-19 16:07:09 +0530

    Made changes in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   trailingSlash: true,
   basePath: "/lp-showcase",
-  output: "export",
-
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   images: {
