27dd685270e8ba0a5e1148c9332d521640c01f14
vivekweberon <vivek@weberon.net>
2024-07-18 16:19:27 +0530

    Fixed the Description Component

--- components/Description.jsx
+++ components/Description.jsx
@@ -1,28 +1,69 @@
-// Description.jsx
-
-import React from "react";
-import ReactMarkdown from "react-markdown";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
-import CustomAnchor from "./CustomAnchor";
+import MarkdownIt from "markdown-it";
+import MarkdownItAnchor from "markdown-it-anchor";
 
 const Description = ({ content, onLinkClick }) => {
-  console.log("CONTENT", content);
-  const renderAnchor = ({ children, href }) => {
-    console.log("renderAnchor href:", href);
-    return (
-      <CustomAnchor href={href} onLinkClick={onLinkClick}>
-        {children}
-      </CustomAnchor>
+  useEffect(() => {
+    const md = new MarkdownIt({
+      html: true,
+      breaks: true,
+    }).use(MarkdownItAnchor);
+
+    // Replace javascript: links with standard URLs
+    const processedContent = content.replace(
+      /\[([^\]]+)\]\(javascript:openModal\('([^']+)'\)\)/g,
+      (match, text, url) => `[${text}](${url})`
     );
-  };
+
+    const renderedContent = md.render(processedContent);
+
+    const handleAnchorClick = (event) => {
+      event.preventDefault();
+      const url = event.target.href;
+      onLinkClick(url);
+    };
+
+    const descriptionElement = document.getElementById("dContent");
+    if (descriptionElement) {
+      descriptionElement.innerHTML = renderedContent;
+
+      const anchors = descriptionElement.getElementsByTagName("a");
+      Array.from(anchors).forEach((anchor) => {
+        anchor.addEventListener("click", handleAnchorClick);
+      });
+    }
+
+    return () => {
+      if (descriptionElement) {
+        const anchors = descriptionElement.getElementsByTagName("a");
+        Array.from(anchors).forEach((anchor) => {
+          anchor.removeEventListener("click", handleAnchorClick);
+        });
+      }
+    };
+  }, [content, onLinkClick]);
 
   return (
-    <div id="description" className="container-fluid">
+    <div
+      id="description"
+      className="container-fluid"
+      style={{
+        paddingTop: "50px",
+        paddingBottom: "50px",
+        backgroundColor: "#fafafa",
+      }}
+    >
+      <div className="row justify-content-center">
+        <div className="col-10" style={{ textAlign: "center" }}>
+          <h1 id="descriptionST"> Description </h1>
+        </div>
+      </div>
       <div className="row justify-content-center">
         <div className="col-10">
-          <ReactMarkdown components={{ a: renderAnchor }}>
-            {content}
-          </ReactMarkdown>
+          <div id="dContent">
+            {/* Rendered markdown content will be inserted here */}
+          </div>
         </div>
       </div>
     </div>
