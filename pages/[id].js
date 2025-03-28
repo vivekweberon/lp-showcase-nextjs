c8eb8137ad25885ed2cc99e27cea179f3d0ce4a3
vivekweberon <vivek@weberon.net>
2025-03-28 18:00:38 +0530

    Added the iframe_api script

--- pages/[id].js
+++ pages/[id].js
@@ -233,6 +233,7 @@ const PropertyPage = ({ propertyData, images }) => {
       <Script src={`${basePath}/js/showdown-1.9.1.min.js`} strategy="beforeInteractive" />
       <Script src={`${basePath}/js/bootstrap.min.js`} strategy="beforeInteractive" />
       <Script src={`${basePath}/js/ytvideo.js`} strategy="beforeInteractive" />
+      <script src="https://www.youtube.com/iframe_api"></script>
 
       <Navbar menu={menuItems} forwardedRef={navbarRef} />
       {sections}
