b05d405aa5b5bad22abcd04779ceb8696a8e5a26
vivekweberon <vivek@weberon.net>
2025-04-09 14:03:28 +0530

    Removing the NextLink from the Showcase component

--- components/Showcase.jsx
+++ components/Showcase.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { basePath } from "@/next.config.js";
-import Link from "next/link";
 
 const Showcase = ({ showcase }) => {
   const { properties, sectionTitle, menu } = showcase;
-  // console.log("Showcase menu", menu);
-  // console.log("Showcase properties:", properties);
+  
   return (
     <div
       id={menu ? menu.replace(/\s/g, '').toLowerCase() : 'showcase'}
@@ -33,15 +31,15 @@ const Showcase = ({ showcase }) => {
             <div className="card">
               <img
                 srcSet={`
-    ${basePath + "/" + "data" + "/" + property.listingPageURL + "/" + property.url}?width=360 360w,
-    ${basePath + "/" + "data" + "/" + property.listingPageURL + "/" + property.url}?width=576 576w,
-    ${basePath + "/" + "data" + "/" + property.listingPageURL + "/" + property.url}?width=768 768w,
-    ${basePath + "/" + "data" + "/" + property.listingPageURL + "/" + property.url}?width=992 992w,
-    ${basePath + "/" + "data" + "/" + property.listingPageURL + "/" + property.url}?width=1200 1200w,
-    ${basePath + "/" + "data" + "/" + property.listingPageURL + "/" + property.url}?width=1400 1400w,
-    ${basePath + "/" + "data" + "/" + property.listingPageURL + "/" + property.url}?width=1600 1600w,
-    ${basePath + "/" + "data" + "/" + property.listingPageURL + "/" + property.url}?width=1920 1920w
-  `}
+                  ${basePath + "/" + "data" + "/" + property.listingPageURL + "/" + property.url}?width=360 360w,
+                  ${basePath + "/" + "data" + "/" + property.listingPageURL + "/" + property.url}?width=576 576w,
+                  ${basePath + "/" + "data" + "/" + property.listingPageURL + "/" + property.url}?width=768 768w,
+                  ${basePath + "/" + "data" + "/" + property.listingPageURL + "/" + property.url}?width=992 992w,
+                  ${basePath + "/" + "data" + "/" + property.listingPageURL + "/" + property.url}?width=1200 1200w,
+                  ${basePath + "/" + "data" + "/" + property.listingPageURL + "/" + property.url}?width=1400 1400w,
+                  ${basePath + "/" + "data" + "/" + property.listingPageURL + "/" + property.url}?width=1600 1600w,
+                  ${basePath + "/" + "data" + "/" + property.listingPageURL + "/" + property.url}?width=1920 1920w
+                `}
                 sizes="(max-width: 600px) 576px, (max-width: 768px) 768px, (max-width: 992px) 992px, (max-width: 1200px) 1200px, (max-width: 1400px) 1400px, (max-width: 1600px) 1600px, (max-width: 1920px) 1920px, 2000px"
                 src={`${basePath + "/" + property.listingPageURL + "/" + property.url}?width=1920`}
                 alt="Property"
@@ -55,11 +53,15 @@ const Showcase = ({ showcase }) => {
                   <div style={{ fontWeight: "lighter" }}>
                     {property.bedsAndBaths}
                   </div>
-                  <div style={{ fontWeight: "lighter" }}>{property.price}</div>
-                  <Link href={property.listingPageURL} style={{ textDecoration: "underline" }}>
+                  <div style={{ fontWeight: "lighter" }}>
+                    {property.price}
+                  </div>
+                  <a 
+                    href={property.listingPageURL} 
+                    style={{ textDecoration: "underline" }}
+                  >
                     Learn More
-                  </Link>
-
+                  </a>
                 </div>
               </div>
             </div>
@@ -71,18 +73,20 @@ const Showcase = ({ showcase }) => {
 };
 
 Showcase.propTypes = {
-  properties: PropTypes.arrayOf(
-    PropTypes.shape({
-      url: PropTypes.string.isRequired,
-      addressLine1: PropTypes.string.isRequired,
-      addressLine2: PropTypes.string.isRequired,
-      bedsAndBaths: PropTypes.string.isRequired,
-      price: PropTypes.string.isRequired,
-      listingPageURL: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-  sectionTitle: PropTypes.string.isRequired,
-  navbarMenu: PropTypes.string.isRequired,
+  showcase: PropTypes.shape({
+    properties: PropTypes.arrayOf(
+      PropTypes.shape({
+        url: PropTypes.string.isRequired,
+        addressLine1: PropTypes.string.isRequired,
+        addressLine2: PropTypes.string.isRequired,
+        bedsAndBaths: PropTypes.string.isRequired,
+        price: PropTypes.string.isRequired,
+        listingPageURL: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+    sectionTitle: PropTypes.string.isRequired,
+    menu: PropTypes.string,
+  }).isRequired,
 };
 
 export default Showcase;
