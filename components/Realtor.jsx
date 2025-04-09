2f0dab56fc063d877cf3bf5ddccc3af902cea30f
vivekweberon <vivek@weberon.net>
2025-04-09 19:12:50 +0530

    Refactored realtor component

--- components/Realtor.jsx
+++ components/Realtor.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { basePath } from "@/next.config.js";
-import PropTypes from "prop-types";
 
 const Realtor = ({ realtor }) => {
-  console.log("Realtor1212", realtor);
-  const { photo, name, company, id, phone, logo, sectionTitle , menu } = realtor;
+  const { photo, name, company, id, phone, logo, sectionTitle, rFooterLink, rFooterLinkText, menu } = realtor;
 
   return (
     <div
@@ -25,7 +23,7 @@ const Realtor = ({ realtor }) => {
           <img
             id="rImage"
             srcSet={`${basePath + "/" + photo}?width=360 360w, ${basePath + "/" + photo}?width=576 576w, ${basePath + "/" + photo}?width=768 768w, ${basePath + "/" + photo}?width=992 992w, ${basePath + "/" + photo}?width=1200 1200w, ${basePath + "/" + photo}?width=1400 1400w, ${basePath + "/" + photo}?width=1600 1600w, ${basePath + "/" + photo}?width=1920 1920w`}
-            sizes="(max-width: 600px) 576px, (max-width: 768px) 768px, (max-width: 992px) 992px, (max-width: 1200px) 1200px, (max-width: 1400px) 1400px, (max-width: 1600px) 1600px, (max-width: 1920px) 1920px, 2000px"
+            sizes="(max-width: 360px) 360px, (max-width: 575px) 540px, 30vw"
             src={`${basePath + "/" + photo}?width=1920`}
             style={{ width: "100%" }}
             alt={name}
@@ -39,17 +37,18 @@ const Realtor = ({ realtor }) => {
           <h3 id="rCompany">{company}</h3>
           <h3 id="rId">{id}</h3>
           <h3>
-            <a
+            {phone && <a
+              id="rPhone"
               href={`tel:${phone}`} // Valid href for telephone link
               style={{ color: "darkblue", textDecoration: "underline" }}
             >
-              {phone}
-            </a>
+              Ph: {phone}
+            </a>}
           </h3>
           <img
             id="rLogo"
             src={basePath + "/" + logo}
-            alt={name}
+            alt="Logo"
             style={{ width: "50%" }}
           />
         </div>
@@ -57,22 +56,14 @@ const Realtor = ({ realtor }) => {
       <div
         id="rFooter"
         className="row no-gutters justify-content-center"
-        style={{ display: "none", marginTop: "20px" }}
-      ></div>
+        style={{ marginTop: "20px" }}
+      >
+			  <div className="col-12" style={{textAlign: center}}> 
+			    <h3 id="rFooterText"><a id="rFooterLink" href={rFooterLink} style={{color: darkblue, textDecoration: underline}}>{rFooterLinkText}</a></h3>
+		    </div>
+			</div>
     </div>
   );
-};
-
-Realtor.propTypes = {
-  realtorData: PropTypes.shape({
-    photo: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    company: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
-    phone: PropTypes.string.isRequired,
-    logo: PropTypes.string.isRequired,
-    sectionTitle: PropTypes.string.isRequired,
-  }).isRequired,
 };
 
 export default Realtor;
