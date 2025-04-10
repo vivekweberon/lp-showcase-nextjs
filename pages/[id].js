fd7c494fbc39379f6b8bb550d17cdd2214466632
vivekweberon <vivek@weberon.net>
2025-04-10 18:18:25 +0530

    Checking listinPageUrl

--- pages/[id].js
+++ pages/[id].js
@@ -330,6 +330,7 @@ export async function getStaticProps(context) {
       props: {
         propertyData: mergedData,
         images: imageUrls,
+        listingPageURL: id,
       },
     };
   } catch (error) {
