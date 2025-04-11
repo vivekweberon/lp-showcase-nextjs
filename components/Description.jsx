72f9cd21d04ba78f46bdf556268bc2dd2cb92b7f
vivekweberon <vivek@weberon.net>
2025-04-11 17:35:15 +0530

    Refactoring Description and Modal component - 4

--- components/Description.jsx
+++ components/Description.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "@/components/Modal";
 import MarkdownIt from "markdown-it";
 import MarkdownItAnchor from "markdown-it-anchor";
