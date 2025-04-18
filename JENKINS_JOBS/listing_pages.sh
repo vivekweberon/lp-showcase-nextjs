daad57ccd043333c5dc60ac5ba6e0a6f2f286120
vivekweberon <vivek@weberon.net>
2025-04-18 13:09:28 +0530

    Testing git log file creation and deployment

--- JENKINS_JOBS/listing_pages.sh
+++ JENKINS_JOBS/listing_pages.sh
@@ -7,22 +7,21 @@ BUILD_TOOL_REPO_DIR="build-tool-repo"
 DEPLOYMENT_REPO_DIR="deployment-repo"
 OUTPUT_DIR="output"
 
+cd "$WORKSPACE" || { echo "Error: Couldn't access workspace directory"; exit 1; }
+
 createVersionFiles() {
   local repo_dir="$1"
   local git_archival_file="git_archival_$repo_dir.txt"
 
   cd "$WORKSPACE/$repo_dir" || { echo "Error: Could not access $repo_dir"; return 1; }
 
-  # For Git repositories
   if [ -d ".git" ]; then
     git show --oneline -s | awk '{print $1}' > "$WORKSPACE/$repo_dir/$git_archival_file"
     echo "Created $git_archival_file in $repo_dir"
   fi
   
-  cd "$WORKSPACE" # Go back to the workspace
+  cd "$WORKSPACE" 
 }
-
-cd "$WORKSPACE" || { echo "Error: Couldn't access workspace directory"; exit 1; }
 
 echo "Cloning input data repository..."
 git clone -b $DCS_DATA_REPO "https://$GITHUB_USERNAME:$GITHUB_TOKEN@$DATA_REPO" $DATA_REPO_DIR || { echo "Failed to clone input data repository"; exit 1; }
@@ -121,6 +120,15 @@ setupDeploymentRepo() {
     echo "Data has been saved to $output_file"
 }
 
+copyVersionFiles(){
+    # Copy the created text files to the final repository directory
+    cp -- "$WORKSPACE/$DATA_REPO_DIR/git_archival_$DATA_REPO_DIR.txt" "$WORKSPACE/$OUTPUT_DIR/$website"
+    cp -- "$WORKSPACE/$BUILD_TOOL_REPO_DIR/git_archival_$BUILD_TOOL_REPO_DIR.txt" "$WORKSPACE/$OUTPUT_DIR/$website"
+    # cp -- "$WORKSPACE/$CODE_REPO_DIR/git_archival_listing_pages.txt" "$WORKSPACE/$OUTPUT_DIR/$website"
+    cp -- "$WORKSPACE/$MAUTIC_TRACKER_REPO_DIR/git_archival_$MAUTIC_TRACKER_REPO_DIR.txt" "$WORKSPACE/$OUTPUT_DIR/$website"
+    echo "Copied archival text files to $DEPLOYMENT_REPO_DIR"
+}
+
 copyWebsiteToGithubRepo() {
 
     cd "$WORKSPACE"
@@ -136,15 +144,9 @@ copyWebsiteToGithubRepo() {
         if [ -d "$WORKSPACE/$OUTPUT_DIR/$website" ]; then
             # Remove any existing copy in $DEPLOYMENT_REPO_DIR, then copy the website build from $CODE_REPO_DIR
             rm -rf "$website"
+            copyVersionFiles
             cp -r "$WORKSPACE/$OUTPUT_DIR/$website" .
             echo "Copied directory '$website' from $OUTPUT_DIR to $DEPLOYMENT_REPO_DIR."
-            
-            # Copy the created text files to the final repository directory
-            cp -- "$WORKSPACE/$DATA_REPO_DIR/git_archival_$DATA_REPO_DIR.txt" "$WORKSPACE/$OUTPUT_DIR/$website"
-            cp -- "$WORKSPACE/$BUILD_TOOL_REPO_DIR/git_archival_$BUILD_TOOL_REPO_DIR.txt" "$WORKSPACE/$OUTPUT_DIR/$website"
-            # cp -- "$WORKSPACE/$CODE_REPO_DIR/git_archival_listing_pages.txt" "$WORKSPACE/$OUTPUT_DIR/$website"
-            cp -- "$WORKSPACE/$MAUTIC_TRACKER_REPO_DIR/git_archival_$MAUTIC_TRACKER_REPO_DIR.txt" "$WORKSPACE/$OUTPUT_DIR/$website"
-            echo "Copied archival text files to $DEPLOYMENT_REPO_DIR"
         else
             echo "Directory '$website' does not exist under $OUTPUT_DIR"
             exit 1
