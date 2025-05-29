08a5061576a7abc672f3eb71be83537fbe208d42
vivekweberon <vivek@weberon.net>
2025-05-29 14:40:12 +0530

    Testing the rollbar config with the new client token

--- rollbar.js
+++ rollbar.js
@@ -1,17 +1,10 @@
+// rollbarClient.js
 import Rollbar from 'rollbar';
 
-const baseConfig = {
+const rollbar = new Rollbar({
+  accessToken: process.env.NEXT_PUBLIC_ROLLBAR_ACCESS_TOKEN,
   captureUncaught: true,
   captureUnhandledRejections: true,
-  environment: process.env.NODE_ENV,
-};
-
-export const clientConfig = {
-  accessToken: process.env.NEXT_PUBLIC_ROLLBAR_CLIENT_TOKEN,
-  ...baseConfig,
-};
-
-export const serverInstance = new Rollbar({
-  accessToken: process.env.ROLLBAR_SERVER_TOKEN,
-  ...baseConfig,
 });
+
+export default rollbar;
