cc752bfa987b4bd2420840a787771bca35b1554f
vivekweberon <vivek@weberon.net>
2025-06-05 17:41:02 +0530

    Testing the rollbar script

--- pages/index.js
+++ pages/index.js
@@ -74,11 +74,6 @@ export async function getStaticProps() {
 function HomePage({ homeData }) {
   useEffect(() => {
     console.log("HomePage component mounted");
-    try {
-      throw new Error("Test error: HomePage useEffect runtime exception");
-    } catch (err) {
-      rollbar.error(err);
-    }
   }, []);
   
   const { page, showcase, contact, realtor, footer, chatbot, homePageSectionsOrder } = homeData;
@@ -177,10 +172,10 @@ function addContact(contact) {
           rel="stylesheet"
           href={`${basePath}/css/lpStyle.css`}
         />
+      {/* <script src="https://cdn.rollbar.com/rollbarjs/refs/tags/v2.22.0/rollbar.min.js"></script> */}
+      </Head>
       <script src={`${basePath}/js/rb-config.js`}></script>
       <script src="https://cdn.rollbar.com/rollbarjs/refs/tags/v2.22.0/rollbar.min.js"></script>
-      </Head>
-      {/* <script src="https://cdn.rollbar.com/rollbarjs/refs/tags/v2.22.0/rollbar.min.js"></script> */}
       <script src={`${basePath}/js/logger.js`}></script>
       <Script src={`${basePath}/js/jquery-3.5.1.min.js`} strategy="beforeInteractive" />
       <Script src={`${basePath}/js/jwt-decode.js`} strategy="beforeInteractive" />
