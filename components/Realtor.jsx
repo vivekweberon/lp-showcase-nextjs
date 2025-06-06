9a803d18cbf609b9f3addb7e70ed723fa9665a1a
vivekweberon <vivek@weberon.net>
2025-06-06 13:37:49 +0530

    Testing onError

--- components/Realtor.jsx
+++ components/Realtor.jsx
@@ -61,7 +61,7 @@ const Realtor = ({ realtor }) => {
             src={basePath + logo}
             alt="Logo"
             style={{ width: "50%" }}
-            onerror="logResourceLoadError(this)"
+            onerror={console.log("Error loading logo image")}
           />
         </div>
       </div>
